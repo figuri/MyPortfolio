6736e16f2817baf4cc4033e42065a24b88f1b36c
unknown <samtisme@gmail.com>
2023-09-02 11:35:37 -0700

    added contact component

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,8 +1,49 @@
 export default function Contact() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setIsSubmitted(true);
+    };
+
     return (
-        <div>
-            <h1>Contact</h1>
-            <p>Contact</p>
-        </div>
+        <section id='contact'>
+            <h2>Contact</h2>
+            {/* confirm message on form submit */}
+            {isSubmitted && <p>Thank you for your message!</p>}
+            {/* contact form */}
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='name'>Name:</label>
+                <input
+                    type='text'
+                    name='name'
+                    id='name'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                />
+                <label htmlFor='email'>Email:</label>
+                <input
+                    type='email'
+                    name='email'
+                    id='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <label htmlFor='message'>Message:</label>
+                <textarea
+                    name='message'
+                    id='message'
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    required
+                />
+                <button type='submit'>Submit</button>
+            </form>
+        </section>
     )
 }
