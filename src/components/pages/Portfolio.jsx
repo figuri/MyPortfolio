73cf40528ead55caca6bc1669a343c68e3aa683e
unknown <samtisme@gmail.com>
2023-09-02 12:29:29 -0700

    addded component for portfolio

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,8 +1,51 @@
 export default function Portfolio() {
+    const projects = [
+        {
+            title: 'Weather forecast app',
+            description: 'This is a weather forecast app using front-end technologies',
+            link: 'https://figuri.github.io/4-cast-app/',
+            repoLink: 'https://github.com/figuri/4-cast-app',
+        },
+        {
+            title: 'Password Generator',
+            description: 'This is a password generator using front-end technologies',
+            link: 'https://figuri.github.io/YoushallnotPass/',
+            repoLink: 'https://github.com/figuri/YoushallnotPass',
+        },
+        {
+            title: 'Work Day Scheduler',
+            description: 'This is a work day scheduler using front-end technologies',
+            link: 'https://figuri.github.io/PLANIT/',
+            repoLink: 'https://github.com/figuri/PLANIT',
+        },
+        {
+            title: 'Tech Blog',
+            descripttion: 'this is a tech blog that utilizes both front-end and back-end technologies',
+            link: '',
+            repoLink: 'https://github.com/figuri/HogBloggin',
+        },
+        {
+            title: 'E-Commerce Back End',
+            description: 'this is the backend for a employee registry for an ecommmerce company',
+            link: 'This is a backend application there is no site to visit but if you visit the github repo and read the readme you can see how to use it !',
+            repoLink: 'https://github.com/figuri/Bemploy',
+        }
+    ]
     return (
-        <div>
-            <h1>Portfolio</h1>
-            <p>Portfolio</p>
+      <section id='portfolio'>
+        <h2>Portfolio</h2>
+        <div className='projects'>
+            {projects.map((project, index) => (
+                <div className='project' key={index}>
+                    <h3>{project.title}</h3>
+                    <p>{project.description}</p>
+                    <div className='links'>
+                        <a href='{project.link}'>Visit Site</a>
+                        <a href='{project.repoLink}'>Visit Repo</a>
+                    </div>
+                </div>
+            ))}
         </div>
+      </section>
     )
 };
