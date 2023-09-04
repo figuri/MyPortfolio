a0b476df4c8b183879b0ebd019644be4f5d2c014
unknown <samtisme@gmail.com>
2023-09-03 17:35:42 -0700

    added styling to my resume page

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -5,35 +5,35 @@ export default function Portfolio() {
             description: 'This is a weather forecast app using front-end technologies',
             link: 'https://figuri.github.io/4-cast-app/',
             repoLink: 'https://github.com/figuri/4-cast-app',
-            image: './public/project-images/4-cast-app.png'
+            image: './project-images/4-cast-app.png'
         },
         {
             title: 'You-Shall-Not-Pass',
             description: 'This is a password generator using front-end technologies',
             link: 'https://figuri.github.io/YoushallnotPass/',
             repoLink: 'https://github.com/figuri/YoushallnotPass',
-            image: './public/project-images/password-generator.png'
+            image: './project-images/password-generator.png'
         },
         {
             title: 'Planit',
             description: 'This is a work day scheduler using front-end technologies',
             link: 'https://figuri.github.io/PLANIT/',
             repoLink: 'https://github.com/figuri/PLANIT',
-            image: './public/project-images/planit.png'
+            image: './project-images/planit.png'
         },
         {
             title: 'Hog Bloggin',
             description: 'This is a front-back end blog app.',
             link: '',
             repoLink: 'https://github.com/figuri/HogBloggin',
-            image: './public/project-images/hog-bloggin.png'
+            image: './project-images/hog-bloggin.png'
         },
         {
             title: 'Bemploy',
             description: 'this is the backend for a employee registry for an ecommmerce company',
             link: 'This is a backend application there is no site to visit but if you visit the github repo and read the readme you can see how to use it !',
             repoLink: 'https://github.com/figuri/Bemploy',
-            image: './public/project-images/bemploy.png'
+            image: './project-images/bemploy.png'
         }
     ]
     return (
--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,74 +1,78 @@
 export default function Resume() {
-    // list of my dev skills as an array
-    const skills = [
-        'JavaScript',
-        'HTML',
-        'CSS',
-        'React',
-        'Node',
-        'Express',
-        'MySQL',
-        'MongoDB',
-        'Mongoose',
-        'Sequelize',
-        'GraphQL',
-        'Apollo',
-        'REST API',
-    ];
-    // list of my experiences as an array
-    const experiences = [
-        {
-            title: 'None',
-            description: ' I am looking forward to my first ever job in the tech industry.',
-        }
-    ]
-    // list of my education as an array
-    const education = [
-        {
-            degree: 'Full Stack Web Development',
-            school: 'University of California Berkeley Extension',
-            year: 'Graduated September 2023',
-        },
-        {
-            degree: 'High School Diploma',
-            school: 'Heritage High School',
-            year: 'Graduated June 2018',
-        }
-    ]
-    // return the JSX for the Resume page
-    return (
-        <section id='resume'>
-            <h2 className="header">Resume</h2>
-            {/** Skills */}
-            <div className='skills'>
-                <h3>Skills</h3>
-                <ul>
-                    {skills.map((skill) => (
-                        <li key={skill}>{skill}</li>
-                    ))}
-                </ul>
+  // list of my dev skills as an array
+  const skills = [
+    "JavaScript",
+    "HTML",
+    "CSS",
+    "React",
+    "Node",
+    "Express",
+    "MySQL",
+    "MongoDB",
+    "Mongoose",
+    "Sequelize",
+    "GraphQL",
+    "Apollo",
+    "REST API",
+  ];
+  // list of my experiences as an array
+  const experiences = [
+    {
+      title: "None",
+      description:
+        " I am looking forward to my first ever job in the tech industry.",
+    },
+  ];
+  // list of my education as an array
+  const education = [
+    {
+      degree: "Full Stack Web Development",
+      school: "University of California Berkeley Extension",
+      year: "Graduated September 2023",
+    },
+    {
+      degree: "High School Diploma",
+      school: "Heritage High School",
+      year: "Graduated June 2018",
+    },
+  ];
+  // return the JSX for the Resume page
+  return (
+    <section id="resume">
+      <h2 className="header">Resume</h2>
+      {/** Skills */}
+      <div className="resume">
+        <div className="skills">
+          <h3 className="title">Skills</h3>
+          <ul className="skillList">
+            {skills.map((skill) => (
+              <li key={skill}>{skill}</li>
+            ))}
+          </ul>
+        </div>
+
+        {/** Experience */}
+        <div className="experience">
+          <h3>Experience</h3>
+          {experiences.map((experience) => (
+            <div key={experience.title}>
+              <h4>{experience.title}</h4>
+              <p>{experience.description}</p>
             </div>
-            {/** Experience */}
-            <div className='experience'>
-                <h3>Experience</h3>
-                {experiences.map((experience) => (
-                    <div key={experience.title}>
-                        <h4>{experience.title}</h4>
-                        <p>{experience.description}</p>
-                    </div>
-                ))}
+          ))}
+        </div>
+        {/** Education */}
+        <div className="education">
+          <h3>Education</h3>
+          {education.map((education) => (
+            <div key={education.school}>
+              <h4>{education.degree}</h4>
+              <p>{education.school}</p>
+              <p>{education.year}</p>
             </div>
-            {/** Education */}
-            <div className='education'>
-                <h3>Education</h3>
-                {education.map((education) => (
-                    <div key={education.school}>
-                        <h4>{education.degree}</h4>
-                        <p>{education.school}</p>
-                        <p>{education.year}</p>
-                    </div>
-                ))}
-            </div>
-        </section>
-    )
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
