80deb6356548ea5c835b9e912dc10caa988bcd1d
unknown <samtisme@gmail.com>
2023-09-05 20:07:01 -0700

    deployed and fixed typo in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer>
       <div className="footer-content">
-        <p className='footer-message'>You can find me on github and linked in, just click the links to the left.</p>
+        <p className='footer-message'>You can find me on github and linked in, just click the links to the right.</p>
         <div className="social-links">
           <a href="https://github.com/figuri">Github</a>
           <a href="https://www.linkedin.com/in/samuel-thomas-b82614183/">LinkedIn</a>
